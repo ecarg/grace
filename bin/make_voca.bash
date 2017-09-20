037bd48329cb15e0e1b0b7f39d696d15148b3580
Jamie <jamie.lim@kakaocorp.com>
2017-09-20 22:27:33 +0900

    v2 코퍼스를 이용해 각종 사전 갱신 #59

--- bin/make_voca.bash
+++ bin/make_voca.bash
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 basedir=$(dirname "$0")
+input_file=${basedir}/../data/v2.train
+output_dir=${basedir}/../rsc
 
 # 음절단위 입력 사전 
-${basedir}/make_voca.py --input=${basedir}/../data/corpus.train\
-    --unit=syllable --type=in --output=${basedir}/../rsc/voca.syl.in
+${basedir}/make_voca.py --input=${input_file} \
+    --unit=syllable --type=in --output=${output_dir}/voca.syl.in
 
 # 음절단위 출력 사전
-${basedir}/make_voca.py --input=${basedir}/../data/corpus.train\
-    --unit=syllable --type=out --output=${basedir}/../rsc/voca.syl.out
+${basedir}/make_voca.py --input=${input_file} \
+    --unit=syllable --type=out --output=${output_dir}/voca.syl.out
 
 # 자소단위 입력 사전
-${basedir}/make_voca.py --input=${basedir}/../data/corpus.train\
-    --unit=phonemes --type=in  --output=${basedir}/../rsc/voca.pho.in
+${basedir}/make_voca.py --input=${input_file} \
+    --unit=phonemes --type=in  --output=${output_dir}/voca.pho.in
 
 # 자소단위 출력 사전(음절단위와 동일하다)
-${basedir}/make_voca.py --input=${basedir}/../data/corpus.train\
-    --unit=phonemes --type=out  --output=${basedir}/../rsc/voca.pho.out
-
-
-
+${basedir}/make_voca.py --input=${input_file} \
+    --unit=phonemes --type=out  --output=${output_dir}/voca.pho.out
