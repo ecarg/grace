83f4a988e2ac66207bf983fe99229870bd772796
Jamie <jamie.lim@kakaocorp.com>
2017-09-26 15:11:55 +0900

    v0.10, v0.11 실험 결과 추가 #76

--- logdir/build_tensorboard.py
+++ logdir/build_tensorboard.py
@@ -12,10 +12,9 @@ __copyright__ = 'No copyright. Just copyleft!'
 # imports #
 ###########
 import argparse
-import codecs
+import glob
 import logging
 import os
-import shutil
 import sys
 import time
 
@@ -30,24 +29,18 @@ def run(args):
     run function which is the start point of program
     :param  args:  arguments
     """
-    file_name = os.path.basename(args.input)
-    if not file_name.endswith('.tsv'):
-        raise ValueError('input file is not *.tsv file: %s', file_name)
-
+    sys.stdin = open('%s/log.tsv' % args.in_dir, 'rt')
     header = sys.stdin.readline().rstrip('\r\n')
     if header != 'iter\tloss\taccuracy\tf-score':
         raise ValueError('invalid log header: %s', header)
 
-    file_dir = os.path.dirname(args.input)
-    if not file_dir:
-        file_dir = '.'
-    model_id = file_name[:-len('.tsv')]
-    model_dir = '%s/%s' % (file_dir, model_id)
-    if os.path.exists(model_dir):
-        logging.info('rebuilding log: %s', model_dir)
-        shutil.rmtree(model_dir)
+    events_files = glob.glob('%s/events.out.tfevents.*' % args.in_dir)
+    if events_files:
+        logging.info('rebuilding log: %s', args.in_dir)
+        for events_file in events_files:
+            os.remove(events_file)
         time.sleep(1)
-    sum_wrt = SummaryWriter(model_dir)
+    sum_wrt = SummaryWriter(args.in_dir)
 
     for line in sys.stdin:
         line = line.rstrip('\r\n')
@@ -76,12 +69,10 @@ def main():
     """
     parser = argparse.ArgumentParser(description='학습 과정에서 출력한 텍스트 로그 파일을 기반으로'
                                                  ' TensorBoard 로그를 만든다.')
-    parser.add_argument('-i', '--input', help='input file', metavar='FILE', required=True)
+    parser.add_argument('-i', '--in-dir', help='input directory', metavar='DIR', required=True)
     parser.add_argument('--debug', help='enable debug', action='store_true')
     args = parser.parse_args()
 
-    if args.input:
-        sys.stdin = codecs.open(args.input, 'r', encoding='UTF-8')
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
     else:
