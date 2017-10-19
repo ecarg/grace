4efb446f198ce4e3a76aeb85de20ffb41e62b0c9
Jamie <jamie.lim@kakaocorp.com>
2017-10-19 22:15:06 +0900

    엑소브레인 Ver 3.0 코퍼스를 반영하여 학습 코퍼스 버전 업 v3 및 리소스 업데이트 #83

--- bin/make_voca.bash
+++ bin/make_voca.bash
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 basedir=$(dirname "$0")
-input_file=${basedir}/../data/v2.train
+input_file=${basedir}/../data/v3.train
 output_dir=${basedir}/../rsc
 
 # 음절단위 입력 사전 
 ${basedir}/make_voca.py --input=${input_file} \
     --unit=syllable --type=in --output=${output_dir}/voca.syl.in
 
-# 음절단위 출력 사전
+# 출력 사전
 ${basedir}/make_voca.py --input=${input_file} \
-    --unit=syllable --type=out --output=${output_dir}/voca.syl.out
+    --unit=syllable --type=out --output=${output_dir}/voca.out
 
 # 자소단위 입력 사전
 ${basedir}/make_voca.py --input=${input_file} \
     --unit=phonemes --type=in  --output=${output_dir}/voca.pho.in
-
-# 자소단위 출력 사전(음절단위와 동일하다)
-${basedir}/make_voca.py --input=${input_file} \
-    --unit=phonemes --type=out  --output=${output_dir}/voca.pho.out
